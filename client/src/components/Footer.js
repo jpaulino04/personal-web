8fcf8ea5d94abcf9219cea61ec77490d50e1481e
Jose Paulino <thenicedeveloper@gmail.com>
2020-04-17 21:21:03 -0400

    updated react packages to the latest and added dynamic year to footer update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
 
 export default function Footer() {
+  let [year, setYear] = useState('')
+
+  let current_Date = new Date()
+  let myYear = current_Date.getFullYear()
+  useEffect(() => {
+    setYear(myYear)
+  }, [])
+  
   return (
     <div className="bg-dark pt-2">
       <p className="text-info text-center">Contact: jpaulino04@msn.com</p>
-      <footer className="text-white p-2 text-center">&copy; 2019</footer>
+      <footer className="text-white p-2 text-center">&copy; {year}</footer>
     </div>
   );
 }
